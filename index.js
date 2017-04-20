07afa66fbb8c94db1f2810a61818c9e52561910d
mkuzmik <mateusz.kuzmik1@gmail.com>
2017-04-20 21:12:00 +0200

    fix

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json())
 
 // Index route
 app.get('/', function (req, res) {
-res.send('Hello world, I am a chat bot')
+res.send('947861287')
 })
 
 // for Facebook verification
