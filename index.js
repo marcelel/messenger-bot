0c13230def7ca013bd2f22314b38902a34c9146c
mkuzmik <mateusz.kuzmik1@gmail.com>
2017-04-20 21:24:25 +0200

    fix

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ console.log('running on port', app.get('port'))
 
 app.get('/webhook', function(req, res) {
   if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === <VERIFY_TOKEN>) {
+      req.query['hub.verify_token'] === 'EAACZA55PYf7wBANVX3HiuWdeBbds0C8QPUkEFpzZCizZCbjfW8lrcYcF6CwOL0WhfH7KE8Yy4yqdvDFZCAiFiEn6pVsE3y0wRd5HzZB19vd0LwcVa3qo1FUYKwlZBsgRmlTlJkZAs3L3Srb0FF9HrZBcihdcUFhvoiAlfZCbVKt7IDwZDZD') {
     console.log("Validating webhook");
     res.status(200).send(req.query['hub.challenge']);
   } else {
